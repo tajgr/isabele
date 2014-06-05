b4342b2d85685374480a0f56d127867daa5aa4c3
tajgr <jlev@tf.czu.cz>
2014-06-05 16:16:05 +0200

    first test

--- fre_task4.py
+++ fre_task4.py
@@ -30,6 +30,10 @@ from airrace import saveIndexedImage, FRAMES_PER_INDEX
 import cvideo
 
 
+MAX_ALLOWED_SPEED = 0.8
+MAX_ALLOWED_VIDEO_DELAY = 2.0 # in seconds, then it will wait (desiredSpeed = 0.0)
+
+
 def timeName( prefix, ext ):
     dt = datetime.datetime.now()
     filename = prefix + dt.strftime("%y%m%d_%H%M%S.") + ext
@@ -39,6 +43,12 @@ def getOrNone():
     if g_queueResults.empty():
         return None
     return g_queueResults.get()
+
+
+g_pave = None
+g_processingEnabled = True # shared multiprocess variable (!)
+
+g_img = None
 
 
 def wrapper( packet ):
@@ -65,6 +75,27 @@ def wrapper( packet ):
                     result = processFrame( frame, debug=False )
                 return (frameNumber( header ), timestamp(header)), result
         header,payload = g_pave.extract()
+
+
+def downloadOldVideo( drone, timeout ):
+    "download video if it is delayed more than MAX_VIDEO_DELAY"
+    global g_processingEnabled
+    restoreProcessing = g_processingEnabled
+    print "Waiting for video to start ..."
+    startTime = drone.time
+    while drone.time < startTime + timeout:
+        if drone.lastImageResult != None:
+            (frameNumber, timestamp), rects = drone.lastImageResult
+            videoTime = correctTimePeriod( timestamp/1000., ref=drone.time )
+            videoDelay = drone.time - videoTime
+            print videoDelay
+            if videoDelay < MAX_ALLOWED_VIDEO_DELAY:
+                print "done"
+                break
+            g_processingEnabled = False
+        drone.update()
+    g_processingEnabled = restoreProcessing
+
 
 g_queueResults = multiprocessing.Queue()
 
@@ -93,8 +124,29 @@ class FreTask4Drone( ARDrone2 ):
 def processFrame( frame, debug=False ):
     pass
 
-def freTask4( ):
-    pass
+def freTask4(drone):
+    drone.speed = 0.1
+    maxVideoDelay = 0.0
+    maxControlGap = 0.0
+    desiredSpeed = MAX_ALLOWED_SPEED
+
+    try:
+        drone.wait(1.0)
+        drone.setVideoChannel( front=False )
+        downloadOldVideo( drone, timeout=20.0 )
+        drone.takeoff()
+        drone.hover(20.0)
+
+    except ManualControlException, e:
+        print "ManualControlException"
+        if drone.ctrlState == 3: # CTRL_FLYING=3 ... i.e. stop the current motion
+            drone.hover(0.1)
+    drone.land()
+    drone.wait(1.0)
+    drone.stopVideo()
+    print "MaxVideoDelay", maxVideoDelay
+    print "MaxControlGap", maxControlGap
+    print "Battery", drone.battery
 
 
 if __name__ == "__main__":
